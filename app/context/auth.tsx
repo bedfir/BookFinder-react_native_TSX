5e1f9c66bb2f25e7d9c74c3aecf613d4e85aa999
Bedoui <firas-bedoui@hotmail.fr>
2023-08-26 01:24:53 +0200

    feat: remove fake data

--- app/context/auth.tsx
+++ app/context/auth.tsx
@@ -1,10 +1,11 @@
 import { useRouter, useSegments } from "expo-router";
 import React, { createContext, useContext, useEffect, useState } from "react";
+import { User } from "../../models/user/user";
 
 interface AuthContextValue {
-    signIn: () => void;
+    signIn: (user: User) => void;
     signOut: () => void;
-    user: any;
+    user: User | null;
 }
 
 const AuthContext = createContext<AuthContextValue | null>(null);
@@ -17,20 +18,20 @@ export function useAuth(): AuthContextValue {
     return context;
 }
 
-function useProtectedRoute(user: any): void {
+function useProtectedRoute(user: User | null): void {
     const segments = useSegments();
     const router = useRouter();
 
     useEffect(() => {
-        const inAuthGroup = segments[0] === "(auth)";
+    const inAuthGroup = segments[0] === "(auth)";
 
-        if (!user && !inAuthGroup) {
-        // If the user is not signed in and the initial segment is not in the auth group.
+    if (!user && !inAuthGroup) {
+      // If the user is not signed in and the initial segment is not in the auth group.
         router.replace("/(auth)/sign-in");
-        } else if (user && inAuthGroup) {
-        // If the user is signed in and the initial segment is in the auth group.
+    } else if (user && inAuthGroup) {
+      // If the user is signed in and the initial segment is in the auth group.
         router.replace("/");
-        }
+    }
     }, [user, segments, router]);
 }
 
@@ -39,13 +40,21 @@ interface ProviderProps {
 }
 
 export function Provider({ children }: ProviderProps): JSX.Element {
-    const [user, setAuth] = useState<any>(null);
+    const [user, setAuth] = useState<User | null>(null);
 
     useProtectedRoute(user);
 
+    const signIn = (user: User) => {
+        setAuth(user);
+    };
+
+    const signOut = () => {
+        setAuth(null);
+    };
+
     const value: AuthContextValue = {
-        signIn: () => setAuth({}),
-        signOut: () => setAuth(null),
+        signIn,
+        signOut,
         user,
     };
 
